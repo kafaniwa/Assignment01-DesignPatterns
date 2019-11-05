9ac28530f4b7de9589a0d578eae0b45954453665
Anson Cheng <ansonrcheng@gmail.com>
2019-11-05 18:20:08 -0500

    Quest Management System Done

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -11,6 +11,7 @@ public class Playermovement : MonoBehaviour
 
     Vector3 moveDir = Vector3.zero;
 
+    TutorialText accessTutorialText;
     CharacterController controller;
     Animator anim;
 
@@ -19,6 +20,7 @@ public class Playermovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        accessTutorialText = GameObject.Find("TutorialText").GetComponent<TutorialText>();
     }
 
     // Update is called once per frame
@@ -32,12 +34,26 @@ public class Playermovement : MonoBehaviour
                 moveDir = new Vector3(0, 0, 1);
                 moveDir *= speed;
                 moveDir = transform.TransformDirection(moveDir);
+
+                if (accessTutorialText.stageNum == 1f)
+                { accessTutorialText.updateText(accessTutorialText.stageNum, 1f); }
             }
 
             if (Input.GetKeyUp(KeyCode.W))
             {
                 anim.SetInteger("Condition", 0);
                 moveDir = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                if (accessTutorialText.stageNum == 1.2f)
+                { accessTutorialText.updateText(accessTutorialText.stageNum, 1.2f); }
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                if (accessTutorialText.stageNum == 1.4f)
+                { accessTutorialText.updateText(accessTutorialText.stageNum, 1.4f); }
             }
         }
         rot += Input.GetAxis("Horizontal") * rotspeed * Time.deltaTime;
